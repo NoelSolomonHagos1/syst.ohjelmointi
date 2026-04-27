2307c5da56b7744b2c24a3f8bd48d49f6fd0f9f9
Noel-lut <noel.solomon.hagos@student.lut.fi>
2026-04-27 03:32:36 +0300

    fix comment

--- project2/my-grep.c
+++ project2/my-grep.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[]) {
     
     char *searchterm = argv[1];
 
-    //no files given
+    //only search term given
     if(argc == 2) {
         search(stdin, searchterm);
         return 0;
